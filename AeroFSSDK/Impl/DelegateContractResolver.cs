dde63be41217607967384f4c3223ab869944b45e
Alex Tsay <alex@aerofs.com>
2015-10-15 13:31:06 -0700

    Initial commit for the AeroFS SDK.

---
+++ AeroFSSDK/Impl/DelegateContractResolver.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Newtonsoft.Json.Serialization;
+
+namespace AeroFSSDK.Impl
+{
+    class DelegateContractResolver : IContractResolver
+    {
+        public IDictionary<Type, IContractResolver> Resolvers { get; set; }
+        public IContractResolver Default { get; set; }
+            = new DefaultContractResolver();
+
+        public JsonContract ResolveContract(Type type)
+        {
+            IContractResolver resolver = Resolvers.TryGetValue(type, out resolver) ? resolver : Default;
+            return resolver.ResolveContract(type);
+        }
+    }
+}
---
+++ AeroFSSDKTests/TestDeleteFolder.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Net;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AeroFSSDK.Tests
+{
+    [TestClass]
+    public class TestDeleteFolder : BaseAPITest
+    {
+        [TestMethod]
+        public void ShouldDeleteFolder()
+        {
+            var folderID = Client.CreateFolder(FolderID.Root, "TestDeleteFolder").ID;
+            Client.GetFolder(folderID);
+            Client.DeleteFolder(folderID);
+
+            ExpectError(HttpStatusCode.NotFound, () => Client.GetFolder(folderID));
+        }
+
+        [TestMethod]
+        public void ShouldDeleteFolderRecursively()
+        {
+            var folderID = Client.CreateFolder(FolderID.Root, "TestDeleteFolder").ID;
+            var childFile = Client.CreateFile(folderID, "ChildFile").ID;
+            var childFolder = Client.CreateFolder(folderID, "ChildFolder").ID;
+            var grandchildFile = Client.CreateFile(childFolder, "GrandchildFile").ID;
+            Client.DeleteFolder(folderID);
+
+            ExpectError(HttpStatusCode.NotFound, () => Client.GetFolder(folderID));
+            ExpectError(HttpStatusCode.NotFound, () => Client.GetFile(childFile));
+            ExpectError(HttpStatusCode.NotFound, () => Client.GetFolder(childFolder));
+            ExpectError(HttpStatusCode.NotFound, () => Client.GetFile(grandchildFile));
+        }
+
+        [TestMethod]
+        public void ShouldThrowBadRequestOnDeleteRootFolder()
+        {
+            var root = Client.GetFolder(FolderID.Root).ID;
+
+            ExpectError(HttpStatusCode.BadRequest, () => Client.DeleteFolder(FolderID.Root));
+            ExpectError(HttpStatusCode.BadRequest, () => Client.DeleteFolder(root));
+        }
+
+        [TestMethod]
+        public void ShouldThrowOnDeleteNonExistantFolder()
+        {
+            var folderID = Client.CreateFolder(FolderID.Root, "TestDeleteFolder").ID;
+            Client.DeleteFolder(folderID);
+
+            ExpectError(HttpStatusCode.NotFound, () => Client.DeleteFolder(folderID));
+        }
+    }
+}
