3377dd6446f3de441bd799ede53ea77f516a2f79
Daniel Cressman <danielcressman@hotmail.com>
2016-01-12 11:30:36 -0800

    Removed values from Settings file.

--- AeroFSSDKTests/Settings.cs
+++ AeroFSSDKTests/Settings.cs
@@ -10,10 +10,10 @@ namespace AeroFSSDK.Tests
         // Need to provide values here to configure tests
         // The host name of the appliance against which you want to run tests
         // e.g. "https://share.aerofs.com"
-        public const string HostName = "https://share.syncfs.com";
+        public const string HostName = "";
 
         // The target API version of the appliance, e.g. "1.4"
-        public const string APIVersion = "1.4";
+        public const string APIVersion = "";
 
         // NOTE: tests will wipe the AeroFS folder for the user against which tests are run
         // A valid access token to run file/folder etc. tests with
@@ -21,13 +21,13 @@ namespace AeroFSSDK.Tests
         // Token should NOT have organization.admin scope (which is the case by default)
         // If tests are failing with a 503 HTTP status, check if this is the case
         // e.g. "9dk4955c0407459d97b2c833ce08dbe3"
-        public const string AccessToken = "9cf4955c0407459d97b2c833ce08dbe3";
+        public const string AccessToken = "";
 
         // A valid ORG ADMIN access token to run tests with
         // Obtain by completing OAuth flow with the 'organization.admin' scope (and all others)
         // Token must have organization.admin scope to run certain tests
         // If tests are failing with permission errors, check that this token has proper scopes
         // e.g. "9dk4955c0407459d97b2c833ce08dbe3"
-        public const string OrgAdminAccessToken = "fecc9b0da0b94f3cb6dafc73988b0efc";
+        public const string OrgAdminAccessToken = "";
     }
 }
